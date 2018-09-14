7eac21b467ba7416a8aede07f9be25d29a7d3d8b
Jesse Loftus <jesse.loftus@gmail.com>
2018-09-14 16:51:34 -0700

    added dynamite kitchen app. cleaned up contact me section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,93 +1,102 @@
 const portfolio = [
   {
-      name: 'Burger Bonzai',
-      image: './assets/images/burgerBonzai.PNG',
-      description: 'Burger Bonzai is a full-stack web-app using MySQL, Node, Express, Handlebars and a homemade ORM.',
-      technologies:'',
-      live: 'https://enigmatic-lowlands-17209.herokuapp.com/',
-      github: 'https://github.com/loftusjl/burger',
-      types: ['fullStack']
+    name: 'Dynamite Kitchen Supplier',
+    image: './assets/images/dks.png',
+    description: `Dynamite Kitchen Supplier (DkS) is a full-stack web-app that will allow for role based users to input orders for purchase using a restaurant kitchen as its model. The app allow's for a seperation of roles each with their own set of privileges and access. Users will need to login or create a new user which will be stored in the database. Username: supervisor, Password: password`,
+    technologies: '',
+    live: 'https://serene-forest-68972.herokuapp.com/',
+    github: 'https://github.com/loftusjl/Dynamite-Kitchen-Supplier',
+    types: ['fullStack']
   },
   {
-      name: 'Friend Finder',
-      image: './assets/images/friendfinder.jpg',
-      description: 'Friend Finder is a full-stack web-app that will compare your survey results with those of 200 users and return the name and profile pic of the people who match your responses most closely!',
-      technologies:'',
-      live: 'https://frozen-river-95808.herokuapp.com/',
-      github: 'https://github.com/loftusjl/FriendFinder',
-      types: ['fullStack']
+    name: 'Burger Bonzai',
+    image: './assets/images/burgerBonzai.PNG',
+    description: 'Burger Bonzai is a full-stack web-app using MySQL, Node, Express, Handlebars and a homemade ORM.',
+    technologies: '',
+    live: 'https://enigmatic-lowlands-17209.herokuapp.com/',
+    github: 'https://github.com/loftusjl/burger',
+    types: ['fullStack']
   },
   {
-      name: 'Bamazon Node App',
-      image: './assets/images/bamazonButton.PNG',
-      description: "An Amazon-like storefront using MySQL, NodeJS, and Javascript. The app takes in orders from customers and depletes stock from the store's inventory. It will also track product sales across store departments and then provide a summary of the highest-grossing departments in the store along with various other functions.",
-      technologies:'',
-      live: '',
-      github: 'https://github.com/loftusjl/bamazon',
-      types: ['backEnd']
+    name: 'Friend Finder',
+    image: './assets/images/friendfinder.jpg',
+    description: 'Friend Finder is a full-stack web-app that will compare your survey results with those of 200 users and return the name and profile pic of the people who match your responses most closely!',
+    technologies: '',
+    live: 'https://frozen-river-95808.herokuapp.com/',
+    github: 'https://github.com/loftusjl/FriendFinder',
+    types: ['fullStack']
   },
   {
-      name: 'The Destinator',
-      image: './assets/images/travelMap01.jpg',
-      description: 'A destination search site filtered by country and then desired city. The Destinator gives you a 5 day forecast, highest ranked hotels, restaurants, and points of interest in the surrounding area.',
-      technologies:'',
-      live: 'https://loftusjl.github.io/TheDestinator/',
-      github: 'https://github.com/loftusjl/TheDestinator',
-      types: ['fullStack']
+    name: 'Bamazon Node App',
+    image: './assets/images/bamazonButton.PNG',
+    description: "An Amazon-like storefront using MySQL, NodeJS, and Javascript. The app takes in orders from customers and depletes stock from the store's inventory. It will also track product sales across store departments and then provide a summary of the highest-grossing departments in the store along with various other functions.",
+    technologies: '',
+    live: '',
+    github: 'https://github.com/loftusjl/bamazon',
+    types: ['backEnd']
   },
   {
-      name: 'LIRI Node App',
-      image: './assets/images/CLI.png',
-      description: 'A NodeJS command line tool for pulling tweets or movie and song info.',
-      technologies:'',
-      live: '',
-      github: 'https://github.com/loftusjl/liri-node-app',
-      types: ['backEnd']
+    name: 'The Destinator',
+    image: './assets/images/travelMap01.jpg',
+    description: 'A destination search site filtered by country and then desired city. The Destinator gives you a 5 day forecast, highest ranked hotels, restaurants, and points of interest in the surrounding area.',
+    technologies: '',
+    live: 'https://loftusjl.github.io/TheDestinator/',
+    github: 'https://github.com/loftusjl/TheDestinator',
+    types: ['fullStack']
   },
   {
-      name: 'Crystal Lazor Gunship!',
-      image: './assets/images/crystalGame.png',
-      description: 'Each game your crystals receive a random value. Click a crystal and try to remember the value. Each click adds to your score. Try to hit the target score without going over!',
-      technologies:'',
-      live: 'https://loftusjl.github.io/Crystal-Lazor-Gunship/',
-      github: 'https://github.com/loftusjl/Crystal-Lazor-Gunship',
-      types: ['frontEnd']
+    name: 'LIRI Node App',
+    image: './assets/images/CLI.png',
+    description: 'A NodeJS command line tool for pulling tweets or movie and song info.',
+    technologies: '',
+    live: '',
+    github: 'https://github.com/loftusjl/liri-node-app',
+    types: ['backEnd']
   },
   {
-      name: 'Horror Hangman',
-      image: './assets/images/hangmanChucky.jpg',
-      description: 'Try to guess the villain in this horror movie take on the classic game hangman!',
-      technologies:'',
-      live: 'https://loftusjl.github.io/WordGuessGame/',
-      github: 'https://github.com/loftusjl/WordGuessGame',
-      types: ['frontEnd']
+    name: 'Crystal Lazor Gunship!',
+    image: './assets/images/crystalGame.png',
+    description: 'Each game your crystals receive a random value. Click a crystal and try to remember the value. Each click adds to your score. Try to hit the target score without going over!',
+    technologies: '',
+    live: 'https://loftusjl.github.io/Crystal-Lazor-Gunship/',
+    github: 'https://github.com/loftusjl/Crystal-Lazor-Gunship',
+    types: ['frontEnd']
   },
   {
-      name: 'HIPAA Quiz',
-      image: './assets/images/HIPAA.png',
-      description: 'A HIPAA training quiz which provides a knowledge check for HIPAA training. Questions are randomized and you areonly given so much time to answer. You must score 70% or greater to pass.',
-      technologies:'',
-      live: 'https://loftusjl.github.io/TriviaGame/',
-      github: 'https://github.com/loftusjl/TriviaGame',
-      types: ['frontEnd']
+    name: 'Horror Hangman',
+    image: './assets/images/hangmanChucky.jpg',
+    description: 'Try to guess the villain in this horror movie take on the classic game hangman!',
+    technologies: '',
+    live: 'https://loftusjl.github.io/WordGuessGame/',
+    github: 'https://github.com/loftusjl/WordGuessGame',
+    types: ['frontEnd']
   },
   {
-      name: 'Giphy Topics',
-      image: './assets/images/giphyGuy.png',
-      description: 'A Giphy image grabber using the Giphy API. Grab images from the site and display them in thumbnail sized images based on pre-selected topics or topics you enter. Images will toggle animation on click. **Warning: This site is loud**',
-      technologies:'',
-      live: 'https://loftusjl.github.io/GiphySearch/',
-      github: 'https://github.com/loftusjl/GiphySearch',
-      types: ['frontEnd']
+    name: 'HIPAA Quiz',
+    image: './assets/images/HIPAA.png',
+    description: 'A HIPAA training quiz which provides a knowledge check for HIPAA training. Questions are randomized and you areonly given so much time to answer. You must score 70% or greater to pass.',
+    technologies: '',
+    live: 'https://loftusjl.github.io/TriviaGame/',
+    github: 'https://github.com/loftusjl/TriviaGame',
+    types: ['frontEnd']
   },
   {
-      name: 'Train Schedule',
-      image: './assets/images/trainSchedule.jpg',
-      description: 'Enter your trains with the Train Name, Destination, First Train Time (in military time), and Frequency -- in minutes. The scheduler will keep track of when the trains are due to arrive and alert you about which should be showing up next in relative time.',
-      technologies:'',
-      live: 'https://loftusjl.github.io/TrainSchedule',
-      github: 'https://github.com/loftusjl/TrainSchedule',
-      types: ['frontEnd']
+    name: 'Giphy Topics',
+    image: './assets/images/giphyGuy.png',
+    description: 'A Giphy image grabber using the Giphy API. Grab images from the site and display them in thumbnail sized images based on pre-selected topics or topics you enter. Images will toggle animation on click. **Warning: This site is loud**',
+    technologies: '',
+    live: 'https://loftusjl.github.io/GiphySearch/',
+    github: 'https://github.com/loftusjl/GiphySearch',
+    types: ['frontEnd']
+  },
+  {
+    name: 'Train Schedule',
+    image: './assets/images/trainSchedule.jpg',
+    description: 'Enter your trains with the Train Name, Destination, First Train Time (in military time), and Frequency -- in minutes. The scheduler will keep track of when the trains are due to arrive and alert you about which should be showing up next in relative time.',
+    technologies: '',
+    live: 'https://loftusjl.github.io/TrainSchedule',
+    github: 'https://github.com/loftusjl/TrainSchedule',
+    types: ['frontEnd']
   }
 ];
 
@@ -95,7 +104,7 @@ const portfolio = [
 // https://codepen.io/danielgroen/pen/VeRPOq
 
 document.addEventListener('DOMContentLoaded', function (event) {
-    addProject(portfolio);
+  addProject(portfolio);
   // array with texts to type in typewriter
   var dataText = ["Hi, I'm Jesse.", "Front-End Developer.", "Back-End Developer.", "Full-Stack Developer."];
   // type one text in the typwriter
@@ -202,7 +211,7 @@ function addProject(data) {
       </div>
     </div>
   </div>`
-  newProject.innerHTML = domString;
-  elem.appendChild(newProject);
+    newProject.innerHTML = domString;
+    elem.appendChild(newProject);
   });
 }
