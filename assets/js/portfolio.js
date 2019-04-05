3fbde51319d66ee6c996dbb95c2944160b6bc440
Jesse Loftus <jesse.loftus@gmail.com>
2019-04-05 07:50:09 -0700

    moved css from portfolio JS to the style.css

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -10,15 +10,25 @@ const portfolio = [
     types: ['fullStack']
   },
   {
-    name: 'Burger Bonzai',
-    image: './assets/images/burgerBonzai.PNG',
+    name: 'Crystal Lazor Gunship!',
+    image: './assets/images/crystalGame.png',
     description:
-      'Burger Bonzai is a full-stack web-app using MySQL, Node, Express, Handlebars and a homemade ORM.',
+      'Each game your crystals receive a random value. Click a crystal and try to remember the value. Each click adds to your score. Try to hit the target score without going over!',
     technologies: '',
-    live: 'https://enigmatic-lowlands-17209.herokuapp.com/',
-    github: 'https://github.com/loftusjl/burger',
-    types: ['fullStack']
+    live: 'https://loftusjl.github.io/Crystal-Lazor-Gunship/',
+    github: 'https://github.com/loftusjl/Crystal-Lazor-Gunship',
+    types: ['frontEnd']
   },
+  // {
+  //   name: 'Burger Bonzai',
+  //   image: './assets/images/burgerBonzai.PNG',
+  //   description:
+  //     'Burger Bonzai is a full-stack web-app using MySQL, Node, Express, Handlebars and a homemade ORM.',
+  //   technologies: '',
+  //   live: 'https://enigmatic-lowlands-17209.herokuapp.com/',
+  //   github: 'https://github.com/loftusjl/burger',
+  //   types: ['fullStack']
+  // },
   {
     name: 'Friend Finder',
     image: './assets/images/friendfinder.jpg',
@@ -39,36 +49,27 @@ const portfolio = [
     github: 'https://github.com/loftusjl/bamazon',
     types: ['backEnd']
   },
-  {
-    name: 'The Destinator',
-    image: './assets/images/travelMap01.jpg',
-    description:
-      'A destination search site filtered by country and then desired city. The Destinator gives you a 5 day forecast, highest ranked hotels, restaurants, and points of interest in the surrounding area.',
-    technologies: '',
-    live: 'https://loftusjl.github.io/TheDestinator/',
-    github: 'https://github.com/loftusjl/TheDestinator',
-    types: ['fullStack']
-  },
-  {
-    name: 'LIRI Node App',
-    image: './assets/images/CLI.png',
-    description:
-      'A NodeJS command line tool for pulling tweets or movie and song info.',
-    technologies: '',
-    live: '',
-    github: 'https://github.com/loftusjl/liri-node-app',
-    types: ['backEnd']
-  },
-  {
-    name: 'Crystal Lazor Gunship!',
-    image: './assets/images/crystalGame.png',
-    description:
-      'Each game your crystals receive a random value. Click a crystal and try to remember the value. Each click adds to your score. Try to hit the target score without going over!',
-    technologies: '',
-    live: 'https://loftusjl.github.io/Crystal-Lazor-Gunship/',
-    github: 'https://github.com/loftusjl/Crystal-Lazor-Gunship',
-    types: ['frontEnd']
-  },
+  // {
+  //   name: 'The Destinator',
+  //   image: './assets/images/travelMap01.jpg',
+  //   description:
+  //     'A destination search site filtered by country and then desired city. The Destinator gives you a 5 day forecast, highest ranked hotels, restaurants, and points of interest in the surrounding area.',
+  //   technologies: '',
+  //   live: 'https://loftusjl.github.io/TheDestinator/',
+  //   github: 'https://github.com/loftusjl/TheDestinator',
+  //   types: ['fullStack']
+  // },
+  // {
+  //   name: 'LIRI Node App',
+  //   image: './assets/images/CLI.png',
+  //   description:
+  //     'A NodeJS command line tool for pulling tweets or movie and song info.',
+  //   technologies: '',
+  //   live: '',
+  //   github: 'https://github.com/loftusjl/liri-node-app',
+  //   types: ['backEnd']
+  // },
+
   {
     name: 'Horror Hangman',
     image: './assets/images/hangmanChucky.jpg',
@@ -83,124 +84,42 @@ const portfolio = [
     name: 'HIPAA Quiz',
     image: './assets/images/HIPAA.png',
     description:
-      'A HIPAA training quiz which provides a knowledge check for HIPAA training. Questions are randomized and you areonly given so much time to answer. You must score 70% or greater to pass.',
+      'A HIPAA training quiz which provides a knowledge check for HIPAA training. Questions are randomized and you are only given so much time to answer. You must score 70% or greater to pass.',
     technologies: '',
     live: 'https://loftusjl.github.io/TriviaGame/',
     github: 'https://github.com/loftusjl/TriviaGame',
     types: ['frontEnd']
-  },
-  {
-    name: 'Giphy Topics',
-    image: './assets/images/giphyGuy.png',
-    description:
-      'A Giphy image grabber using the Giphy API. Grab images from the site and display them in thumbnail sized images based on pre-selected topics or topics you enter. Images will toggle animation on click. **Warning: This site is loud**',
-    technologies: '',
-    live: 'https://loftusjl.github.io/GiphySearch/',
-    github: 'https://github.com/loftusjl/GiphySearch',
-    types: ['frontEnd']
-  },
-  {
-    name: 'Train Schedule',
-    image: './assets/images/trainSchedule.jpg',
-    description:
-      'Enter your trains with the Train Name, Destination, First Train Time (in military time), and Frequency -- in minutes. The scheduler will keep track of when the trains are due to arrive and alert you about which should be showing up next in relative time.',
-    technologies: '',
-    live: 'https://loftusjl.github.io/TrainSchedule',
-    github: 'https://github.com/loftusjl/TrainSchedule',
-    types: ['frontEnd']
   }
+  // {
+  //   name: 'Giphy Topics',
+  //   image: './assets/images/giphyGuy.png',
+  //   description:
+  //     'A Giphy image grabber using the Giphy API. Grab images from the site and display them in thumbnail sized images based on pre-selected topics or topics you enter. Images will toggle animation on click. **Warning: This site is loud**',
+  //   technologies: '',
+  //   live: 'https://loftusjl.github.io/GiphySearch/',
+  //   github: 'https://github.com/loftusjl/GiphySearch',
+  //   types: ['frontEnd']
+  // },
+  // {
+  //   name: 'Train Schedule',
+  //   image: './assets/images/trainSchedule.jpg',
+  //   description:
+  //     'Enter your trains with the Train Name, Destination, First Train Time (in military time), and Frequency -- in minutes. The scheduler will keep track of when the trains are due to arrive and alert you about which should be showing up next in relative time.',
+  //   technologies: '',
+  //   live: 'https://loftusjl.github.io/TrainSchedule',
+  //   github: 'https://github.com/loftusjl/TrainSchedule',
+  //   types: ['frontEnd']
+  // }
 ];
 // add projects to portfolio section
 document.addEventListener('DOMContentLoaded', function() {
   let styles = `
-          .pCard {
-              border: 5px solid ${
-                document.getElementById('orange').style.backgroundColor
-              };
-              overflow: hidden;
-              height:150px;
-              transition: max-height 0.6s ease-in-out;
-              -webkit-transition: max-height 0.6s ease-in-out; /* Safari 3.1 to 6.0 */
-            }
-            .pCard[data-clicked='1'] {
-                height: auto;
-            }
-            #p0 {
-                height:auto; 
-              }
-          .pHead {
-            width: 100%;
-            height: 100%;
-            min-width: 100%;
-            min-height: 100%;
-            position: relative;
-            cursor: pointer;
-          }
-          .pHead::before {
-            background-size: cover;
-            content: "";
-            display: block;
-            top:0;
-            left: 0;
-            width: 100%;
-            height: 150px;
-            z-index: -2;
-            opacity: 0.65;
-        }
-        .pHead::after {
-            background-color: #00d400;
-            content: "";
-            display: block;
-            position: absolute;
-            top: 0px;
-            left: 0px;
-            width: 100%;
-            height: 100%;
-            z-index: -1;
-            opacity: 0.4;
-            }
-
-          .textOverlay {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-content: center;
-            
-          }
-          .pHead-text {
-            color: #fff;
-            text-shadow: #343a40 2px 2px;
-            min-width: 100%;
-            min-height: 150px;
-            display: block;
-            position: absolute;
-            top:15px;
-          }
-          .pHead-text::before {
-            content: "";
-            margin-left: 0;
-            min-width: 100%;
-            min-height: 150px;
-            z-index: -1;
-            opacity: 0.4;
-            background-color: #343a40;
-          } 
-          .pHead-title, .pHead-text {
-            width: 100%;
-            display: block;
-            text-align: center;
-          } 
-          .pHead-title {
-            margin: 10px 0;
-            text-transform: uppercase;
-          }
-          .pCard span{
-            margin: 15px;
-            width: 100%;
-          }
-         
-
-      `;
+  .pCard {
+    border: 5px solid ${
+      document.getElementById('orange').style.backgroundColor
+    };
+  }
+  `;
   addProjects(portfolio, styles);
   clickExpand();
 });
